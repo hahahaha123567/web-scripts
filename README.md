ca6cfac841a20273f3291ce94bafcf832dc92a62
ZhangYaoxin <hahahaha123567@qq.com>
2018-08-05 01:59:42 +0800

    persistence time with localStorage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 1. leave bilibili
+
--- leave-bilibili.js
+++ leave-bilibili.js
@@ -18,16 +18,40 @@
     $.post("https://space.bilibili.com/ajax/member/GetInfo", {mid:userId}, function(result){
         data = result.data;
     });
+    let title = "otaku";
+    let body = "再不学习要没饭吃了";
+    let icon = "https://www.bilibili.com/favicon.ico";
+    let link = "https://leetcode-cn.com/problemset/algorithms/";
 
-	// notify
-    setTimeout ( function () {
-        let title = data.name;
-        let body = "再不学习要没饭吃了";
-        let icon = "https://www.bilibili.com/favicon.ico";
-        let link = "https://leetcode-cn.com/problemset/algorithms/";
-        createNotification(title, body, icon, link);
-    }, 1000);
+    localStorage.setItem('testTime', 0)
 
+    setInterval(
+        () => {
+            let time = parseInt(localStorage.getItem('testTime'), 10);
+            time++;
+            localStorage.setItem('testTime', time);
+            if (time < 3) return;
+            title = data.name;
+            let hour = 0, minute = 0, second = 0;
+            second = time % 60;
+            minute = Math.floor(time/60) % 60;
+            hour = Math.floor(time / 60 / 60);
+            body = '今天你在bilibili花了 ';
+            if (hour > 0) {
+                body += hour;
+                body += '小时 ';
+            }
+            if (minute > 0) {
+                body += minute;
+                body += '分钟';
+            }
+            body += second;
+            body += '秒';
+            if (second === 0 && minute > 0 && minute % 10 === 0) {
+                createNotification(title, body, icon, link);
+            }
+        }, 1000
+    );
 })();
 
 function createNotification (title, body, icon, link) {
